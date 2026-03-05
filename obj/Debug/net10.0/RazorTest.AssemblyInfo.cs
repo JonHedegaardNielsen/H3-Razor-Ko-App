852eedaefbbed5ee76f1187b878c0cdff00d5ffd
Jon Hedegaard Nielsen <jon230706@gmail.com>
2026-03-05 10:37:03 +0100

    change file location

--- obj/Debug/net10.0/RazorTest.AssemblyInfo.cs
+++ obj/Debug/net10.0/RazorTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+634f8ceb0d4f982762715340fba1bca3584c1fa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
